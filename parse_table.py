67f489ea32d34e942aa05ba32df7657cac86c87c
twitu <bhanuka.ishan@gmail.com>
2019-03-01 19:30:58 +0530

    Added filters for centred text boxes, paragraphs and table lines

--- parse_table.py
+++ parse_table.py
@@ -21,6 +21,9 @@ class Rectangle:
         self.x2 = max(self.x2, value.x2)
         self.y2 = max(self.y2, value.y2)
 
+    def __str__(self):
+        return " ".join(map(str, [x1, y1, x2, y2]))
+
 
 class Word:
 
@@ -129,7 +132,7 @@ class Paragraph:
     def __gt__(self, value):
         return self.box.y1 > value.box.x1
 
-
+# group words that are horizontally in the same line
 def get_lines(page_element, margin=5):
     lines = []
     page_box = Rectangle(list(map(float, page_element.attrib["bbox"].split(","))))
@@ -139,15 +142,13 @@ def get_lines(page_element, margin=5):
             if not text_line.tag == "textline": continue
             bbox = Rectangle(list(map(float, text_line.attrib["bbox"].split(","))))
             value = ''.join([text_char.text if text_char.text else " " for text_char in text_line.getchildren()])
-            value = value.rstrip()
+            value = value.strip()
             # TODO: filter and correct text value
             word = Word(value, bbox)
-            # if filter_centre_word(page_box, word):
-            #     continue
             added = False
             for line in lines:
                 # check within a margin of error if the text_line can be added in an exisiting line
-                if line.box.y1 - margin < bbox.y1 < line.box.y1 + margin and line.box.y2 - margin < bbox.y2 < line.box.y2 + margin:
+                if (line.box.y1 - margin < bbox.y1 < line.box.y1 + margin) and (line.box.y2 - margin < bbox.y2 < line.box.y2 + margin):
                     line.add_word(word)
                     added = True
             
@@ -184,17 +185,24 @@ def get_columns(lines, margin=10):
                 columns.append(Column(word))
     return columns
 
-def merge_words(line, margin=10):
-    new_line = []
-    merger = line.words[0]
-    for i, word in enumerate(line.words[1:]):
-        if merger.box.x2 + margin > word.box.x1:
-            merger.merge(word)
-        else:
-            new_line.append(merger)
-            merger = word
-    new_line.append(merger)
-    line = new_line
+# merge closely spaced words
+def merge_words(lines, margin=15):
+    new_lines = []
+    for line in lines:
+        merger = line.words[0]
+        new_line = Line(merger)
+        for i, word in enumerate(line.words[1:]):
+            if merger.box.x2 + margin > word.box.x1:
+                merger.merge(word)
+            else:
+                new_line.add_word(merger)
+                merger = word
+        # avoid adding starting word again
+        # happens when line has only one word
+        if merger != new_line.words[0]:
+            new_line.add_word(merger)
+        new_lines.append(new_line)
+    return new_lines
 
 
 def get_paragraphs(lines, margin=5):
@@ -245,12 +253,108 @@ def write_to_csv(lines):
             csvwriter.writerow(csv_line)
 
 
+def centre_aligned(line, page_box, margin=15, cutoff=0.6):
+    mid = (page_box.x1 + page_box.x2)/2
+    width = page_box.x2 - page_box.x1
+
+    if line.box.x1 < mid < line.box.x2:
+        left = mid - line.box.x1
+        right = line.box.x2 - mid
+        if (-margin < left - right < margin) and ((line.box.x2 - line.box.x1)/width < cutoff):
+            return True
+
+    return False
+
     
 if __name__ == "__main__":
     file_name = "para.xml"
     xml_file = tree.parse(file_name).getroot()
     page = xml_file.getchildren()[0]
-    lines = get_lines(page, 5)
+    page_box = Rectangle(list(map(float, page.attrib["bbox"].split(","))))
+    page_mid = (page_box.x1 + page_box.x2)/2
+    page_width = page_box.x2 - page_box.x1
+    lines = get_lines(page)
+    lines = merge_words(lines)
     lines.sort(reverse=True)
-    paras = get_paragraphs(lines)
-    write_to_doc(paras)
+
+    filter_lines = []
+    centre_text = []
+    table_lines = []
+    paragraph_lines = []
+
+    for line in lines:
+        print(line)
+    print()
+
+    # get centre text
+    for line in lines:
+        if centre_aligned(line, page_box):
+            centre_text.append(line)
+        else:
+            filter_lines.append(line)
+    lines = filter_lines
+
+    # get lines with multiple unmerged words
+    # these have high chance of forming tables
+    filter_lines = []
+    for line in lines:
+        if len(line.words) > 1:
+            table_lines.append(line)
+        else:
+            filter_lines.append(line)
+    lines = filter_lines
+
+    # filter large lines that are likely to be part of paragraphs
+    filter_lines = []
+    for line in lines:
+        if (line.box.x2 - line.box.x1)/page_width > 0.6:
+            paragraph_lines.append(line)
+        else:
+            filter_lines.append(line)
+    lines = filter_lines
+
+    for line in lines:
+        print(line)
+    print()
+
+    # get small lines adjacent to table lines even if they have one word
+    # these can be empty entries in the table or headings of entries
+    # add rest of small lines to paragraph lines
+    for line in lines:
+        to_add = False
+        margin = 15
+        for table_line in table_lines:
+            if table_line.box.y1 - margin < line.box.y2 and line.box.y1 < table_line.box.y1 or \
+                table_line.box.y2 + margin > line.box.y1 and line.box.y2 > table_line.box.y2:
+                to_add = True
+
+        if to_add:
+            table_lines.append(line)
+        else:
+            paragraph_lines.append(line)
+
+    # get paragraphs
+    paragraphs = get_paragraphs(paragraph_lines, 10)
+
+    # sort all types of lines
+    paragraphs.sort(reverse=True)
+    table_lines.sort(reverse=True)
+    centre_text.sort(reverse=True)
+
+    for line in table_lines:
+        print(line)
+    print()
+
+    for paragraph in paragraphs:
+        # first line should be bold if its width is less than paragraph width
+        line = paragraph.lines[0]
+        if (line.box.x2-line.box.x1)/(paragraph.box.x2-paragraph.box.x1) < 0.6:
+            print(line)
+
+        for line in paragraph.lines[1:]:
+            print(line.__str__(), sep=" ")
+        print()
+
+    # lines.sort(reverse=True)
+    # paras = get_paragraphs(lines)
+    # write_to_doc(paras)
