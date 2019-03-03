b2edb176db9113f2ac9e6f7a00ceca85d3add3a9
Rahul Bothra <rrbothra@gmail.com>
2019-03-03 16:03:51 +0530

    pep8 fixes

--- generate_readables.py
+++ generate_readables.py
@@ -1,22 +1,25 @@
 import os
 from os.path import join
 
+
 def generate_readables(dir_path):
     '''
     convert all pdfs in dir_path in 3 formats,
     1. text readable pdfs, stored in ocr_pdfs/
     2. txt from pdf, stored in textfiles/
     3. xml from pdf, stored in xml/
-    
+
     Requires pdf2txt.py
     '''
-    
+
     os.chdir(dir_path)
 
     for data_file_name in os.listdir('.'):
         if '_ocr.pdf' in data_file_name:
-            os.system('pdf2txt.py ' + data_file_name + ' > ' + data_file_name.split('.')[0] + '.txt')
-            os.system('pdf2txt.py -t xml ' + data_file_name + ' > ' + data_file_name.split('.')[0] + '.xml')
+            os.system('pdf2txt.py ' + data_file_name + ' > ' +
+                      data_file_name.split('.')[0] + '.txt')
+            os.system('pdf2txt.py -t xml ' + data_file_name +
+                      ' > ' + data_file_name.split('.')[0] + '.xml')
 
     os.mkdir('textfiles')
     os.mkdir('xml')
